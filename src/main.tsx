6f0f1b23d0719c3784d658e4b00b6ba0be35389e
Drmacze DEV <drma801@gmail.com>
2026-08-17 02:11:12 +0700

    Load refined product copy and final navbar spacing

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,20 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import ThemeSystem from './ThemeSystem'
 import PageNavigation from './PageNavigation'
+import CopyRefinement from './CopyRefinement'
 import './styles.css'
 import './brand-logos.css'
 import './mobile-polish.css'
 import './theme-system.css'
 import './polish-v2.css'
 import './page-navigation.css'
+import './nav-copy-fix.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeSystem />
     <PageNavigation />
+    <CopyRefinement />
     <App />
   </StrictMode>,
 )
