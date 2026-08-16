67a8b36bd3722980c18ed672672f3b6cdae23ffa
Drmacze DEV <drma801@gmail.com>
2026-08-17 01:28:11 +0700

    Wire paged navigation into app shell

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import ThemeSystem from './ThemeSystem'
+import PageNavigation from './PageNavigation'
 import './styles.css'
 import './brand-logos.css'
 import './mobile-polish.css'
 import './theme-system.css'
 import './polish-v2.css'
+import './page-navigation.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeSystem />
+    <PageNavigation />
     <App />
   </StrictMode>,
 )
