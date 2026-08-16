b4107a3809232162f3e834189d5d03340534db2c
Drmacze DEV <drma801@gmail.com>
2026-08-17 02:23:39 +0700

    Wire account security system into app shell

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import ThemeSystem from './ThemeSystem'
 import PageNavigation from './PageNavigation'
 import CopyRefinement from './CopyRefinement'
+import AccountSystem from './AccountSystem'
 import './styles.css'
 import './brand-logos.css'
 import './mobile-polish.css'
@@ -11,12 +12,14 @@ import './theme-system.css'
 import './polish-v2.css'
 import './page-navigation.css'
 import './nav-copy-fix.css'
+import './account-system.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeSystem />
     <PageNavigation />
     <CopyRefinement />
+    <AccountSystem />
     <App />
   </StrictMode>,
 )
