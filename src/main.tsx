24d5fd5fee43df17f7955fb8bff20a3ad2781f40
Drmacze DEV <drma801@gmail.com>
2026-08-17 00:35:53 +0700

    Wire theme system into app shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import ThemeSystem from './ThemeSystem'
 import './styles.css'
 import './brand-logos.css'
 import './mobile-polish.css'
+import './theme-system.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ThemeSystem />
     <App />
   </StrictMode>,
 )
