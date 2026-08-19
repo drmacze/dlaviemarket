9f29b00941940baf173a4b17de6f300c7e4d56e9
Drmacze DEV <drma801@gmail.com>
2026-08-20 03:20:44 +0700

    Load v23 digital market styles

--- src/DigitalManualOperatorFallback.tsx
+++ src/DigitalManualOperatorFallback.tsx
@@ -1,4 +1,5 @@
 import DLavieDigitalMarketV23 from './DLavieDigitalMarketV23'
+import './digital-market-v23.css'
 
 export default function DigitalManualOperatorFallback(){
   return <DLavieDigitalMarketV23 />
