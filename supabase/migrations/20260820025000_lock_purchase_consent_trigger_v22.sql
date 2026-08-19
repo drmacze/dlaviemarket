b507f361657948b0b97a45b31e8523cc1be28fcc
Drmacze DEV <drma801@gmail.com>
2026-08-20 02:53:14 +0700

    Lock v22 consent trigger to service role

---
+++ supabase/migrations/20260820025000_lock_purchase_consent_trigger_v22.sql
@@ -0,0 +1,2 @@
+revoke execute on function api.dlavie_require_h2h_purchase_consent_v22() from public, anon, authenticated;
+grant execute on function api.dlavie_require_h2h_purchase_consent_v22() to service_role;
