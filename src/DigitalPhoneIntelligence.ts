32f057a706116c1ff870571afe8e7de273de22d6
Drmacze DEV <drma801@gmail.com>
2026-08-20 03:20:58 +0700

    Improve Indonesian mobile prefix detection for v23

--- src/DigitalPhoneIntelligence.ts
+++ src/DigitalPhoneIntelligence.ts
@@ -38,7 +38,7 @@ export function detectIndonesianOperator(value=''):IndonesianOperator{
   const p=x.slice(0,4)
   if(['0811','0812','0813','0821','0822','0823','0851','0852','0853'].includes(p))return'Telkomsel'
   if(['0814','0815','0816','0855','0856','0857','0858'].includes(p))return'IM3'
-  if(['0817','0818','0819','0877','0878'].includes(p))return'XL'
+  if(['0817','0818','0819','0859','0877','0878'].includes(p))return'XL'
   if(['0831','0832','0833','0838'].includes(p))return'AXIS'
   if(['0895','0896','0897','0898','0899'].includes(p))return'Tri'
   if(['0881','0882','0883','0884','0885','0886','0887','0888','0889'].includes(p))return'Smartfren'
